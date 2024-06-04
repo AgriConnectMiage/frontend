c178d509445d355cb3d40f81fe3a3b226261369c
RemiSaurel <r.saurel5@gmail.com>
2024-06-04 23:17:19 +0200

    feat: add first components

---
+++ components/inventory/field/Container.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Cell from "~/components/inventory/field/Cell.vue";
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 w-1/2 overflow-auto">
+    <div v-for="i in 8" :key="i" class="flex gap-2">
+      <Cell v-for="j in 8" :key="j" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/inventory.vue
+++ pages/inventory.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        <h1>Inventory</h1>
-        <p>Here is the inventory page</p>
+  <div class="w-full">
+    <UButton icon="i-heroicons-question-mark-circle" class="mt-2 mb-4"
+      >How does it work?</UButton
+    >
+    <div class="flex gap-4">
+      <InventoryFieldContainer />
+      <InventoryDeviceContainer />
     </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import InventoryFieldContainer from "~/components/inventory/field/Container.vue";
+import InventoryDeviceContainer from "~/components/inventory/device/Container.vue";
+</script>
