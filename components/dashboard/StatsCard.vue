3a1e33d3657e30e570c981d1c95305f4cc2d872f
RemiSaurel <r.saurel5@gmail.com>
2024-06-08 09:27:00 +0200

    feat: statscard

---
+++ components/dashboard/StatsCard.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  value: Number,
+});
+</script>
+
+<template>
+  <UCard
+    :ui="{
+      base: 'min-w-[200px]',
+      ring: '',
+      divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+      body: {
+        base: '',
+        background: '',
+        padding: 'p-4 sm:p-4',
+      },
+      header: {
+        base: '',
+        background: '',
+        padding: 'p-4 sm:p-4',
+      },
+    }"
+    class="w-fit"
+  >
+    <template #header>
+      <div class="text-xl truncate">
+        {{ props.title }}
+      </div></template
+    >
+    <div class="text-2xl font-bold p-0">
+      {{ props.value }}
+    </div>
+  </UCard>
+</template>
+
+<style scoped></style>
--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <h1>Sandbox</h1>
+  <div class="w-full h-full">
+    <div class="flex flex-wrap gap-4">
+      <DashboardStatsCard
+        v-for="stat in classicStats"
+        :title="stat.title"
+        :value="stat.value"
+      />
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+const classicStats = [
+  { title: "🧑‍🌾 Farmers", value: 12 },
+  { title: "🌾 Fields", value: 12 },
+  { title: "📡 Sensors", value: 28 },
+  { title: "💧 Actuators", value: 25 },
+];
+</script>
